beb636ad45abb63aa4790e8c97908646ad7657ee
maciek zelek <mzelechowski79@gmail.com>
2021-10-16 09:31:35 +0200

    Initial Commit: Basic structure

---
+++ src/pl/mike/Tower.java
@@ -0,0 +1,31 @@
+package pl.mike;
+
+public class Tower {
+    private int damage;
+    private int defence;
+    private int rate;
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+}
